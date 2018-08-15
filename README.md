e197f0d2de3f7bc631d7c87e57ea4e9a629fd490
Ming Ho <hkboi108@hotmail.com>
2018-08-15 16:01:36 -0700

    style: update CSS styling

--- README.md
+++ README.md
@@ -1,6 +1,31 @@
-frontend-nanodegree-arcade-game
-===============================
+# JavaScript Frogger Game
 
-Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
+## How to play
 
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+Use the arrow keys on your keyboard to move your player around! Your goal is to
+reach the water without touching any of the enemy bugs. If your player grazes
+one of the bugs, your player will land back on the grass and you will have to
+start all over, so don't do it.
+
+## Dependencies
+* JavaScript
+* Google Fonts
+
+## License
+This project is licensed under GNU GPLv3.
+
+## Acknowledgments
+
+The following resources really helped me with this project.
+Arcade Game Walkthrough Part 2- Pseudo Code by [Matthew Cranford](https://matthewcranford.com/arcade-game-walkthrough-part-2-pseudo-code/).
+
+http://w3bits.com/javascript-modal/
+
+https://css-tricks.com/using-requestanimationframe/
+
+https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
+
+https://jlongster.com/Making-Sprite-based-Games-with-Canvas
+
+Code for random number generator sourced from:
+https://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range
--- js/app.js
+++ js/app.js
@@ -1,7 +1,5 @@
 // Enemies our player must avoid
 var Enemy = function(x, y, speed) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
@@ -22,11 +20,7 @@ Enemy.prototype.update = function(dt) {
     // which will ensure the game runs at the same speed for
     // all computers.
 
-    //if not at boundary
-      //move forward
-      //increment x by speed*dt
-    //else
-      //reset position to start
+    //if not at boundary, move forward. Otherwise reset position to start.
     if (this.x < this.rightBoundary) {
       this.x += this.speed * dt;
     } else {
@@ -39,31 +33,7 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
-//player class
-  //Constructor
-    //properties
-      //sprite image
-      // x position
-      // y position
-    //methods
-      //update position
-        //check player x and y
-          //if collides with enemy
-            //reset to start position
-          //if position is at top row
-            //you win
-      //render
-        //draw player image based on current x and y pos
-      //handleInput
-        //listen for key presses
-        //update player's x or y pos based on key press
-      //reset
-        //set x and y to starting position
-
+// Create player class to use as blueprint for player object
 class Player {
   constructor() {
     this.sprite = 'images/char-boy.png';
@@ -76,37 +46,41 @@ class Player {
     this.winner = false;
   }
 
-  // draw the player sprite on current x and y position
+  //Draw the player sprite on current x and y position
   render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
   }
 
+  //Update the player's position
+  //The maximum is inclusive and the minimum is inclusive
   update() {
-    function getRandomInt(min, max) {
-      min = Math.ceil(min);
-      max = Math.floor(max);
-      return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
-    }
-
     for (let enemy of allEnemies) {
-      // If the enemy collides with player or enters the same tile as player, reset player's position
+      //If the enemy's right side or left side touches player and enemy is on
+      //same row as player, reset player's position
       if ((enemy.x + enemy.sideStep - 25 > this.x && enemy.x < this.x +
         this.sideStep - 25) && this.y == enemy.y) {
         this.reset();
       }
     }
 
-    // If player reaches the last tile, player wins
+    //If player reaches the top row, player wins
     if (this.y == -35) {
       this.winner = true;
     }
   }
-  // reset player to start position
+
+  //Reset player to start position
   reset() {
     let char = ['images/char-boy.png', 'images/char-cat-girl.png',
     'images/char-horn-girl.png', 'images/char-pink-girl.png',
     'images/char-princess-girl.png'];
-    this.sprite = char[1];
+    const getRandomInt = (min, max) => {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    let randomNum = getRandomInt(0, char.length - 1);
+    this.sprite = char[randomNum];
     this.x = this.startX;
     this.y = this.startY;
     this.winner = false;
@@ -115,33 +89,35 @@ class Player {
   /**
   * @param {string} input - key press that determines travel direction
   */
+  //Update player's x or y position based on key presses
   handleInput(input) {
     switch(input) {
       case 'left':
+        //Checks for left boundary
         if (this.x > 0) {
           this.x -= this.sideStep;
         }
         break;
       case 'up':
+        //Checks for top boundary
         if (this.y > 0) {
           this.y -= this.step;
-          console.log(this.x);
-          console.log(this.y);
         }
         break;
       case 'right':
+        //Checks for right boundary
         if (this.x < this.sideStep * 4) {
           this.x += this.sideStep;
         }
         break;
       case 'down':
+        //Checks for bottom boundary
         if (this.y < this.startY) {
           this.y += this.step;
         }
         break;
      }
   }
-
 };
 
 // Now instantiate your objects.
@@ -153,8 +129,12 @@ let allEnemies = [];
 let enemy1 = new Enemy(-101, 83, 50);
 let enemy2 = new Enemy(-101, 166, 200);
 let enemy3 = new Enemy(-101, 249, 300);
-let enemy4 = new Enemy(-101*1.5, 249, 100);
-allEnemies.push(enemy1, enemy2, enemy3, enemy4);
+let enemy4 = new Enemy(-101*1.5, 83, 250);
+let enemy5 = new Enemy(-101*1.6, 166, 500);
+let enemy6 = new Enemy(-101*1.7, 249, 150);
+let enemy7 = new Enemy(-101*1.7, 249, 250);
+
+allEnemies.push(enemy1, enemy2, enemy3, enemy4, enemy5, enemy6, enemy7);
 
 //initialize player object at start position
 let player = new Player();
