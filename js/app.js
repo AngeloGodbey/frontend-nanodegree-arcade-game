6f0af50c542330dfc3613b026bbbc1ad35677ac9
Ming Ho <hkboi108@hotmail.com>
2018-08-16 13:25:31 -0700

    feat: change enemy direction upon collision

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
+'use strict';
+
 // Enemies our player must avoid
-let Enemy = function(x, y, speed) {
+class Enemy {
+  constructor(x, y, speed) {
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
@@ -8,29 +11,55 @@ let Enemy = function(x, y, speed) {
     this.row = 83;
     this.rightBoundary = this.sideStep * 5;
     this.startPos = -this.sideStep;
+    this.endPos = this.rightBoundary + this.sideStep;
     this.x = x;
     this.y = y - 35;
+    this.rightSide = this.sideStep - 25;
     this.speed = speed;
-};
+    this.collide = false;
+    this.forward = true;
+    this.backward = false;
+  }
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-
-    //if not at boundary, move forward. Otherwise reset position to start.
-    if (this.x < this.rightBoundary) {
-      this.x += this.speed * dt;
-    } else {
-      this.x = this.startPos;
+  // Update the enemy's position, required method for game
+  // Parameter: dt, a time delta between ticks
+  // Multiply any movement by the dt parameter which will ensure the game runs
+  // at the same speed for all computers.
+  update(dt) {
+    // Check if enemy is moving forward or backwards
+    if (this.forward == true) {
+      // If not at right boundary, continue moving forward. Otherwise reset
+      // position to start position.
+      if (this.x < this.rightBoundary) {
+        this.x += this.speed * dt;
+      } else {
+        this.x = this.startPos;
+      }
+    } else {      // If moving backward
+      if (this.x > 0){
+        this.x -= this.speed * dt;
+      } else {
+        // If enemy move past the left boundary, reset to end position.
+        this.x = this.endPos;
+      }
     }
-};
+    // If player collides with enemy, move the opposite direction
+    if (this.collide == true) {
+      if (this.forward == true) {
+        this.backward = true;
+        this.forward = false;
+      } else {
+        this.backward = false;
+        this.forward = true;
+      }
+      this.collide = false;
+    }
+  }
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+  // Draw the enemy on the screen, required method for game
+  render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
 };
 
 // Create player class to use as blueprint for player object
@@ -44,6 +73,7 @@ class Player {
     this.x = this.startX;
     this.y = this.startY;
     this.winner = false;
+    this.leftSide = this.x + this.sideStep - 25;
   }
 
   //Draw the player sprite on current x and y position
@@ -52,14 +82,14 @@ class Player {
   }
 
   //Update the player's position
-  //The maximum is inclusive and the minimum is inclusive
   update() {
     for (let enemy of allEnemies) {
       //If the enemy's right side or left side touches player and enemy is on
       //same row as player, reset player's position
-      if ((enemy.x + enemy.sideStep - 25 > this.x && enemy.x < this.x +
-        this.sideStep - 25) && this.y == enemy.y) {
+      if ((enemy.x + enemy.rightSide > this.x && enemy.x < this.leftSide)
+      && this.y == enemy.y) {
         this.reset();
+        enemy.collide = true;
       }
     }
 
@@ -69,11 +99,13 @@ class Player {
     }
   }
 
-  //Reset player to start position
+  //Reset player to start position and randomly change sprite image
   reset() {
     let char = ['images/char-boy.png', 'images/char-cat-girl.png',
     'images/char-horn-girl.png', 'images/char-pink-girl.png',
     'images/char-princess-girl.png'];
+
+    //The maximum is inclusive and the minimum is inclusive
     const getRandomInt = (min, max) => {
       min = Math.ceil(min);
       max = Math.floor(max);
@@ -124,17 +156,15 @@ class Player {
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-let allEnemies = [];
-//for each enemy, create enemy object and push into array
-let enemy1 = new Enemy(-101, 83, 50);
-let enemy2 = new Enemy(-101, 166, 200);
-let enemy3 = new Enemy(-101, 249, 300);
-let enemy4 = new Enemy(-101*1.5, 83, 250);
-let enemy5 = new Enemy(-101*1.6, 166, 500);
-let enemy6 = new Enemy(-101*1.7, 249, 150);
-let enemy7 = new Enemy(-101*1.7, 249, 250);
-
-allEnemies.push(enemy1, enemy2, enemy3, enemy4, enemy5, enemy6, enemy7);
+let allEnemies = [
+  new Enemy(-101, 83, 50),
+  new Enemy(-101, 166, 200),
+  new Enemy(-101, 249, 300),
+  new Enemy(-101*1.5, 83, 250),
+  new Enemy(-101*1.6, 166, 500),
+  new Enemy(-101*1.7, 249, 150),
+  new Enemy(-101*1.7, 249, 250),
+];
 
 //initialize player object at start position
 let player = new Player();
